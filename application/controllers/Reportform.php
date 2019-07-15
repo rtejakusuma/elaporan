7610adb8cb992d2c4030d3b18b7a1fe67a8ad5bc
marde12345 <mardefasma123up@gmail.com>
2019-07-15 13:47:44 +0700

    controller

--- application/controllers/Reportform.php
+++ application/controllers/Reportform.php
@@ -1,22 +1,24 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Reportform extends CI_Controller {
-    private function isAuth(){
-        if($this->session->tempdata('credential') == NULL){
-			redirect('login');
-		}
+class Reportform extends CI_Controller
+{
+    private function isAuth()
+    {
+        if ($this->session->tempdata('credential') == NULL) {
+            redirect('login');
+        }
     }
 
     public function index()
-	{
+    {
         $this->isAuth();
-		$this->load->view('reportform', array('formname' => NULL));
+        $this->load->view('reportform', array('formname' => NULL));
     }
-    
-    public function f($formname){
+
+    public function f($formname)
+    {
         $this->isAuth();
         $this->load->view('reportform', array('formname' => $formname));
     }
-
 }
