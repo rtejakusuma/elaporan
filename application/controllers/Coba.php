0b4f68430dcb01ba92a4d3a41108a024fd3acea7
marde12345 <mardefasma123up@gmail.com>
2019-07-15 13:57:03 +0700

    coba

---
+++ application/controllers/Coba.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Controllername extends CI_Controller
+{
+
+    public function index()
+    { }
+}
+
+/* End of file Controllername.php */
