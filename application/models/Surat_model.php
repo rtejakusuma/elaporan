557230000f766f5902b43f29fcf769f385c5aa1b
tirta <tirtajiwanggataufiq@gmail.com>
2019-07-18 15:07:45 +0700

    update surat_model

--- application/models/Surat_model.php
+++ application/models/Surat_model.php
@@ -5,17 +5,32 @@ class Surat_model extends CI_Model
 {
     public function gets()
     {
-        return $this->db->get_where('surat')->result();
+        return $this->db->select('surat')->result();
     }
 
-    public function get_surat($id)
+    public function get_surat($id) // get Surat berdasarkan ID
     {
-        return $this->db->get_where($this->_get_tipe_surat($id))->result();
+        $table = $this->get_tipe_surat($id);
+        $temp = $this->db->get($table)->row_array();
+        if($temp == NULL){
+            return -1; // error code
+        }
+        return $temp;
     }
 
-    private function _get_tipe_surat($id)
+    public function get_by_id($id){
+        return $this->db->get_where('surat', array('id_surat' => $id))->result()[0]->id_tipe;
+    }
+
+    public function get_tipe_surat($id){
+        $res = $this->get_by_id($id);
+        return $this->_get_nama_surat($res);
+    }
+
+    private function _get_nama_surat($id) // get Nama Surat berdasarkan tipe surat
     {
-        return $this->db->select('tipe_surat')->get_where('tipe_surat')->result();
+        $temp = $this->db->get_where('tipe_surat', array('id_tipe' => $id))->result();
+        return $temp[0]->nama_surat;
     }
 }
 
