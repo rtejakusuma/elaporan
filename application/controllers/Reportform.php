5147ec972ad7bd73fbba096a38da52a0c53d6113
tirta <tirtajiwanggataufiq@gmail.com>
2019-07-20 13:15:47 +0700

    update reportform suratmodel

--- application/controllers/Reportform.php
+++ application/controllers/Reportform.php
@@ -16,12 +16,35 @@ class Reportform extends CI_Controller
         $this->load->view('reportform', array('formname' => NULL));
     }
 
-    public function f($formname)
+    public function f($formname) // create
     {
         $this->load->model('surat_model');
         $data['fielddata'] = $this->surat_model->get_fielddata($formname);
         $data['formname'] = $formname;
         $this->load->view('reportform', array('data' => $data));
+    }
+
+    public function e($id) // edit
+    {
+        $this->load->model('surat_model', 'surat');
+        $data = $this->surat->get_surat_data($id);
+        $this->load->view('reportform', array('data' => data));
+    }
+
+    public function submit($id = NULL)
+    {
+        $this->load->model('surat_model', 'surat');
+        $data = $this->input->post();
+        // metadata needed:
+        // - nama tipe surat
+        if($id == NULL){ // new data
+            $this->surat->add_data($data);
+        } else { // editted data
+            $this->surat->update_data($id, $data);
+        }
+        
+        // redirect('opd','refresh');
+        
     }
 
     private function sess_ver()
--- application/models/Surat_model.php
+++ application/models/Surat_model.php
@@ -18,17 +18,26 @@ class Surat_model extends CI_Model
         return $temp;
     }
 
-    public function get_by_id($id){
+    public function get_by_id($id)
+    {
         return $this->db->get_where('surat', array('id_surat' => $id))->result()[0]->id_tipe;
     }
 
-    public function get_tipe_surat($id){
+    public function get_tipe_surat($id)
+    {
         $res = $this->get_by_id($id);
         return $this->_get_nama_surat($res);
     }
 
-    public function get_fielddata($name){
+    public function get_fielddata($name)
+    {
         return $this->db->field_data($name);
+    }
+
+    public function get_surat_data($id)
+    {
+        $tipe = $this->get_tipe_surat($id);
+        return $this->db->get_where($tipe, array('id_surat' => $id))->result[0];
     }
 
     private function _get_nama_surat($id) // get Nama Surat berdasarkan tipe surat
@@ -36,6 +45,17 @@ class Surat_model extends CI_Model
         $temp = $this->db->get_where('tipe_surat', array('id_tipe' => $id))->result();
         return $temp[0]->nama_surat;
     }
+
+    public function add_data($data)
+    {
+        
+    }
+
+    public function update_data($id, $data)
+    {
+
+    }
+
 }
 
 /* End of file Surat_model.php */
