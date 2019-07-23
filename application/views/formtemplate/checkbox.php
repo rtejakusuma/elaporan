f23f89ff2baaf78e938b4de2edf045b6260dbbe9
RavenRindou <rtejakusuma21@yahoo.com>
2019-07-23 11:11:03 +0700

    push

---
+++ application/views/formtemplate/checkbox.php
@@ -0,0 +1,54 @@
+<!-- Start to do list -->
+<div class="col-md-12 col-sm-12 col-xs-12">
+              <div class="x_panel">
+                <div class="x_title">
+                  <h2>To Do List <small>Sample tasks</small></h2>
+                  <ul class="nav navbar-right panel_toolbox">
+                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                    </li>
+                    <li><a class="close-link"><i class="fa fa-close"></i></a>
+                    </li>
+                  </ul>
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+
+                  <div class="">
+                    <ul class="to_do">
+                      <li>
+                        <p>
+                          <input type="checkbox" class="flat" id=""> Schedule meeting with new client </p>
+                      </li>
+                      <li>
+                        <p>
+                          <input type="checkbox" class="flat"  id=""> Create email address for new intern</p>
+                      </li>
+                      <li>
+                        <p>
+                          <input type="checkbox" class="flat"  id=""> Have IT fix the network printer</p>
+                      </li>
+                      <li>
+                        <p>
+                          <input type="checkbox" class="flat"  id=""> Copy backups to offsite location</p>
+                      </li>
+                      <li>
+                        <p>
+                          <input type="checkbox" class="flat"  id=""> Food truck fixie locavors mcsweeney</p>
+                      </li>
+                      <li>
+                        <p>
+                          <input type="checkbox" class="flat"  id=""> Food truck fixie locavors mcsweeney</p>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                        <button class="btn btn-primary" type="button">Cancel</button>
+                        <button class="btn btn-primary" type="reset">Reset</button>
+                        <button type="submit" class="btn btn-success">Submit</button>
+                    </div>
+                 </div>
+                </div>
+              </div>
+            </div>
+            <!-- End to do list -->
