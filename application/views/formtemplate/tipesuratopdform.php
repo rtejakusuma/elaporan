9df8f9dc14c2486f133ef837fe3ab366dd4d3043
RavenRindou <rtejakusuma21@yahoo.com>
2019-07-24 09:56:40 +0700

    hapus dropdown

--- application/views/formtemplate/tipesuratopdform.php
+++ application/views/formtemplate/tipesuratopdform.php
@@ -3,12 +3,6 @@
               <div class="x_panel">
                 <div class="x_title">
                   <h2>To Do List <small>Sample tasks</small></h2>
-                  <ul class="nav navbar-right panel_toolbox">
-                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                    </li>
-                    <li><a class="close-link"><i class="fa fa-close"></i></a>
-                    </li>
-                  </ul>
                   <div class="clearfix"></div>
                 </div>
                 <div class="col-md-6 col-sm-6 col-xs-12">
