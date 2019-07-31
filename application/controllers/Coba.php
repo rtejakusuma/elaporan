f6f4621881c49da0b4deb9ae11f2902dc26f739b
marde12345 <mardefasma123up@gmail.com>
2019-08-01 02:22:22 +0700

    api sikd

--- application/controllers/Coba.php
+++ application/controllers/Coba.php
@@ -6,33 +6,33 @@ class Coba extends CI_Controller
 
     public function index()
     {
-        $this->load->model('api_sipp_model');
-        $data = $this->api_sipp_model->get_api('tahun','2020');
+        $this->load->model('api_sikd_model');
+        $data = $this->api_sikd_model->get_api();
         $json = json_encode($data, JSON_PRETTY_PRINT);
 
         return json_decode($json, true);
-        
+
         // printf("<pre>%s</pre>", $json);
         // die();
     }
 
     public function test()
     {
-        $laporan_baru = array('id_laporan'=>'318','id_opd'=>'290','id_tipe'=>'1');
+        $laporan_baru = array('id_laporan' => '318', 'id_opd' => '290', 'id_tipe' => '1');
         $id_opd = '290';
         $tahun = '2020';
         $this->load->model("tipelaporan/realisasifisik_model", 'tt');
         $this->tt->init_insert($id_opd, $laporan_baru, array('tgl' => '2020-09-09'));
         // $this->load->model('api_sipp_model', 'sipp');
         // $this->sipp->api_fetch_data($id_opd, $laporan_baru, $tahun);
-        
+
     }
 
     public function lap($formname, $id)
     {
         $this->load->model('laporan_model', 'lp');
         $this->lp->get_laporan_data_by_name_id($formname, $id);
-    } 
+    }
 
     public function loaddata()
     {
---
+++ application/models/Api_sikd_model.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Api_sikd_model extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->library('xmlrpc');
+        $this->load->library('xmlrpcs');
+    }
+
+
+    public function get_api()
+    {
+        $url = "sikd.madiunkota.net/serapan/serapan2019/api/2";
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 60,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+        ]);
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        if ($err) {
+            // API GAGAL
+            return NULL;
+        } else {
+            // API BERHASIL
+            $arr = json_decode($response, true);
+            return $arr;
+        }
+    }
+}
+
+/* End of file Api_sikd_model.php */
