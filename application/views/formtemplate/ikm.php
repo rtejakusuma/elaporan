2189b21816cff34374b4844e85df8b0d98c0905f
marde12345 <mardefasma123up@gmail.com>
2019-08-06 07:20:57 +0700

    pagi pagi

--- application/views/formtemplate/ikm.php
+++ application/views/formtemplate/ikm.php
@@ -24,7 +24,7 @@
              <div class="col-md-12 col-sm-12 col-xs-12">
                <div class="x_panel">
                  <div class="x_content">
-
+                   <a href='<?php if (isset($data['id_laporan'])) echo base_url("export/$data[formname]/$data[id_laporan]"); ?>' target='_blank'><button class="btn btn-success"><i class="fa fa-print"></i> Excel</button></a>
                    <a href='<?php if (isset($data['id_laporan'])) echo base_url("opd/p/$data[formname]/$data[id_laporan]"); ?>' target='_blank'><button class="btn btn-primary"><i class="fa fa-print"></i> PRINT</button></a>
                    <div class="" role="tabpanel" data-example-id="togglable-tabs">
                      <ul id="myTab" class="nav nav-tabs bar_tabs" role="tablist">
@@ -54,57 +54,58 @@
 
                              <?php if ($data['fetch']['ikmopd'] != NULL) {
                                 foreach ($data['fetch']['ikmopd'] as $ikmopd) {
-                              ?>
-                              <div>
+                                  ?>
+                                 <div>
 
-                              <div class="col-md-12 col-sm-12 col-xs-12" style='border: 2px solid black; padding:10px;'>
-                                <!-- <input value='<?php //echo $ikm['id_temuan']; 
-                                                  ?>' type='hidden' name='id_temuan[]'> -->
-                                <div class='form-group'>
-                                  <label for="opd" class="control-label col-md-3 col-sm-3 col-xs-12">Nama OPD</label>
-                                  <select class="col-md-6 col-sm-6 col-xs-12" name='id_opd[]'>
-                                    <?php
-                                    foreach ($data['opsi_opd'] as $opd) {
-                                      $sel = '';
-                                      if ($ikmopd['id_opd'] == $opd['id_opd']) $sel = "selected='selected'";
-                                      echo "<option value='$opd[id_opd]' $sel>$opd[nama_opd]</option>";
-                                    }
-                                    ?>
-                                  </select>
-                                </div>
+                                   <div class="col-md-12 col-sm-12 col-xs-12" style='border: 2px solid black; padding:10px;'>
+                                     <!-- <input value='<?php //echo $ikm['id_temuan']; 
+                                                        ?>' type='hidden' name='id_temuan[]'> -->
+                                     <div class='form-group'>
+                                       <label for="opd" class="control-label col-md-3 col-sm-3 col-xs-12">Nama OPD</label>
+                                       <select class="col-md-6 col-sm-6 col-xs-12" name='id_opd[]'>
+                                         <?php
+                                          foreach ($data['opsi_opd'] as $opd) {
+                                            $sel = '';
+                                            if ($ikmopd['id_opd'] == $opd['id_opd']) $sel = "selected='selected'";
+                                            echo "<option value='$opd[id_opd]' $sel>$opd[nama_opd]</option>";
+                                          }
+                                          ?>
+                                       </select>
+                                     </div>
 
-                                <div class='form-group'>
-                                  <label for='predikat' class='control-label col-md-3 col-sm-3 col-xs-12'>Predikat</label>
-                                  <div class='col-md-6 col-sm-6 col-xs-12'>
-                                    <input value='<?php echo $ikmopd['predikat']; ?>' class='form-control col-md-7 col-xs-12' type='text' name='predikat[]'>
-                                  </div>
-                                </div>
+                                     <div class='form-group'>
+                                       <label for='predikat' class='control-label col-md-3 col-sm-3 col-xs-12'>Predikat</label>
+                                       <div class='col-md-6 col-sm-6 col-xs-12'>
+                                         <input value='<?php echo $ikmopd['predikat']; ?>' class='form-control col-md-7 col-xs-12' type='text' name='predikat[]'>
+                                       </div>
+                                     </div>
 
-                                <div class='form-group'>
-                                  <label for='nilai' class='control-label col-md-3 col-sm-3 col-xs-12'>Nilai</label>
-                                  <div class='col-md-6 col-sm-6 col-xs-12'>
-                                    <input value='<?php echo $ikmopd['nilai']; ?>' class='form-control col-md-7 col-xs-12' type='text' name='nilai[]'>
-                                  </div>
-                                </div>
+                                     <div class='form-group'>
+                                       <label for='nilai' class='control-label col-md-3 col-sm-3 col-xs-12'>Nilai</label>
+                                       <div class='col-md-6 col-sm-6 col-xs-12'>
+                                         <input value='<?php echo $ikmopd['nilai']; ?>' class='form-control col-md-7 col-xs-12' type='text' name='nilai[]'>
+                                       </div>
+                                     </div>
 
-                                <div class="form-group">
-                                  <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-                                    <button type='button' onclick='delete_node(this)'>Hapus</button>
-                                  </div>
-                                </div>
+                                     <div class="form-group">
+                                       <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                                         <button type='button' onclick='delete_node(this)'>Hapus</button>
+                                       </div>
+                                     </div>
 
-                                </div>
-                                </div>
+                                   </div>
+                                 </div>
 
-                                 <?php }} ?>
-                                </div>
+                               <?php }
+                              } ?>
+                           </div>
 
-                                <div class="ln_solid"></div>
-                                <div class="form-group">
-                                  <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-                                    <button style="position: fixed; bottom: 28px; right: 48px;font-size:20px;  width: 100px;" type="submit" class="btn btn-success" >Submit</button>
-                                </div>
-                                </div>
+                           <div class="ln_solid"></div>
+                           <div class="form-group">
+                             <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                               <button style="position: fixed; bottom: 28px; right: 48px;font-size:20px;  width: 100px;" type="submit" class="btn btn-success">Submit</button>
+                             </div>
+                           </div>
 
                          </form>
                        </div>
@@ -119,8 +120,8 @@
          <div>
 
 
-<script>
-var opd = "<div>\
+           <script>
+             var opd = "<div>\
   <div class='col-md-12 col-sm-12 col-xs-12' style='border: 2px solid black; padding:10px;'>\
   \
   <div class='form-group'>\
@@ -158,16 +159,16 @@ var opd = "<div>\
 </div>\
 ";
 
-function add_field() {
-  var cont = document.getElementById('container-opsi');
-  console.log(cont);
-  cont.innerHTML = opd + cont.innerHTML;
-}
+             function add_field() {
+               var cont = document.getElementById('container-opsi');
+               console.log(cont);
+               cont.innerHTML = opd + cont.innerHTML;
+             }
 
-function delete_node(node) {
-  node.parentNode.parentNode.parentNode.parentNode.removeChild(node.parentNode.parentNode.parentNode);
-}
-</script>
+             function delete_node(node) {
+               node.parentNode.parentNode.parentNode.parentNode.removeChild(node.parentNode.parentNode.parentNode);
+             }
+           </script>
 
 
            <!-- /page content -->
