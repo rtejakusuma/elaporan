d19062e95de120a4207707d639d6bcbe6b5c4712
tirta <tirtajiwanggataufiq@gmail.com>
2019-08-08 13:25:07 +0700

    up backup restore fungsi done

--- application/models/Dbbackup_model.php
+++ application/models/Dbbackup_model.php
@@ -39,22 +39,15 @@ class Dbbackup_model extends CI_Model
 
     public function restore_laporan($formname, $id_laporan, $tm)
     {
-        // $err;
-        // $this->db->trans_start();
-        // $this->db->delete('realisasi_fisik', "id_laporan = $id_laporan");
-        // exec("mysql -upecel -ppecelgratis testing < ". APPPATH . "../migration/$formname/$tm.sql", $err);
-        // // var_dump( $err );
-        // $this->db->trans_complete();
         $query = file_get_contents(APPPATH . "../migration/$formname/$tm.sql");
         $this->db->trans_start();
         $this->db->delete('realisasi_fisik', "id_laporan = $id_laporan");
         foreach(explode(';', $query) as $q){
             // echo $q . "</br>";
             // $q = strval($q);
-            $this->db->query($q);
+            $this->db->simple_query($q);
             
         }
-        // mysqli_multi_queries($query);
         
         $this->db->trans_complete();
     }
